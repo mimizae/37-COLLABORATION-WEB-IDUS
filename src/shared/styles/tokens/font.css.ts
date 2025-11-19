443429c1ebea84aea09f46b684986183303a76bf
mimizae <jim400934@gmail.com>
2025-11-19 16:54:28 +0900

    refactor: 폰트 단위 rem으로 수정

--- src/shared/styles/tokens/font.css.ts
+++ src/shared/styles/tokens/font.css.ts
@@ -1,13 +1,13 @@
 import { createGlobalTheme } from "@vanilla-extract/css";
 
 export const fontSize = createGlobalTheme(":root", {
-  "4xl": "50px",
-  "2xl": "32px",
-  xl: "24px",
-  lg: "18px",
-  base: "16px",
-  sm: "14px",
-  xs: "12px",
+  "4xl": "5rem",
+  "2xl": "3.2rem",
+  xl: "2.4rem",
+  lg: "1.8rem",
+  base: "1.6rem",
+  sm: "1.4rem",
+  xs: "1.2rem",
 });
 
 export const fontWeight = createGlobalTheme(":root", {
