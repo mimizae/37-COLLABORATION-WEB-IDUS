9a21e6438b469b97fc349503e99a8ef484344739
mimizae <jim400934@gmail.com>
2025-11-20 16:27:59 +0900

    feat: 하트 아이콘 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,54 @@
+import * as styles from "./test.css";
+import {
+  ArrowLeft,
+  ArrowRight,
+  Banner,
+  Benefit,
+  BottomSheetHeart,
+  BottomSheetHeartFill,
+  ChatFloating,
+  Check,
+  ChevronDown,
+  ChevronRightRounded,
+  ChevronRightSharp,
+  ChevronUp,
+  Delete,
+  Home,
+  Logo,
+  MakerHeart,
+  MakerHeartFill,
+  Search,
+  Share,
+  ShoppingCart,
+  Star,
+} from "./assets/svg";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className={styles.test}>
+      <MakerHeart width={24} height={24} />
+      <MakerHeartFill width={24} height={24} />
+      <BottomSheetHeart width={24} height={24} />
+      <BottomSheetHeartFill width={50} height={50} />
+      <ArrowLeft width={24} height={24} />
+      <ArrowRight width={24} height={24} />
+      <ChevronDown width={24} height={24} />
+      <ChevronRightRounded width={24} height={24} />
+      <ChevronRightSharp width={24} height={24} />
+      <ChevronUp width={24} height={24} />
+      <Banner width={200} height={200} />
+      <Benefit width={24} height={24} />
+      <ChatFloating width={24} height={24} />
+      <Check width={24} height={24} />
+      <Delete width={24} height={24} />
+      <Home width={24} height={24} />
+      <Logo width={24} height={24} />
+      <Search width={24} height={24} />
+      <Share width={24} height={24} />
+      <ShoppingCart width={24} height={24} />
+      <Star width={24} height={24} />
+    </div>
+  );
 }
 
 export default App;
--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -2,28 +2,31 @@
 // svg 폴더에 추가한 svg 이미지는 index.ts에서 아래와 같은 형식으로 export해 사용해주세요!
 
 // arrow
-export { default as ArrowLeft } from "@svg/arrow/left.svg";
-export { default as ArrowRight } from "@svg/arrow/right.svg";
+export { default as ArrowLeft } from "@svg/arrow/left.svg?react";
+export { default as ArrowRight } from "@svg/arrow/right.svg?react";
 
 // chevron
-export { default as ChevronDown } from "@svg/chevron/down.svg";
-export { default as ChevronRightRounded } from "@svg/chevron/right-rounded.svg";
-export { default as ChevronRightSharp } from "@svg/chevron/right-sharp.svg";
-export { default as ChevronUp } from "@svg/chevron/up.svg";
+export { default as ChevronDown } from "@svg/chevron/down.svg?react";
+export { default as ChevronRightRounded } from "@svg/chevron/right-rounded.svg?react";
+export { default as ChevronRightSharp } from "@svg/chevron/right-sharp.svg?react";
+export { default as ChevronUp } from "@svg/chevron/up.svg?react";
 
 // heart
-export { default as MakerHeart } from "@svg/heart/maker.svg";
-export { default as BottomSheetHeart } from "@svg/heart/bottom-sheet.svg";
+
+export { default as MakerHeartFill } from "@svg/heart/maker-fill.svg?react";
+export { default as MakerHeart } from "@svg/heart/maker.svg?react";
+export { default as BottomSheetHeartFill } from "@svg/heart/bottom-sheet-fill.svg?react";
+export { default as BottomSheetHeart } from "@svg/heart/bottom-sheet.svg?react";
 
 // common
-export { default as Banner } from "@svg/banner.svg";
-export { default as Benefit } from "@svg/benefit.svg";
-export { default as ChatFloating } from "@svg/chat-floating.svg";
-export { default as Check } from "@svg/check.svg";
-export { default as Delete } from "@svg/delete.svg";
-export { default as Home } from "@svg/home.svg";
-export { default as Logo } from "@svg/logo.svg";
-export { default as Search } from "@svg/search.svg";
-export { default as Share } from "@svg/share.svg";
-export { default as ShoppingCart } from "@svg/shopping-cart.svg";
-export { default as Star } from "@svg/star.svg";
+export { default as Banner } from "@svg/banner.svg?react";
+export { default as Benefit } from "@svg/benefit.svg?react";
+export { default as ChatFloating } from "@svg/chat-floating.svg?react";
+export { default as Check } from "@svg/check.svg?react";
+export { default as Delete } from "@svg/delete.svg?react";
+export { default as Home } from "@svg/home.svg?react";
+export { default as Logo } from "@svg/logo.svg?react";
+export { default as Search } from "@svg/search.svg?react";
+export { default as Share } from "@svg/share.svg?react";
+export { default as ShoppingCart } from "@svg/shopping-cart.svg?react";
+export { default as Star } from "@svg/star.svg?react";
