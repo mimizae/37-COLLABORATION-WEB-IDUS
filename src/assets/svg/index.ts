4d95bedf048ca46f3460003b86c8058b64c03bdd
mimizae <jim400934@gmail.com>
2025-11-20 15:36:15 +0900

    fix: 의존성 재설치

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,9 +1,8 @@
 // index.ts: SVG 파일들을 React 컴포넌트로 변환해 일괄적으로 관리
 // svg 폴더에 추가한 svg 이미지는 index.ts에서 아래와 같은 형식으로 export해 사용해주세요!
 
-
 // arrow
-export { default as ArrowLeft } from "@svg/arrow/back.svg";
+export { default as ArrowLeft } from "@svg/arrow/left.svg";
 export { default as ArrowRight } from "@svg/arrow/right.svg";
 
 // chevron
@@ -27,4 +26,4 @@ export { default as Logo } from "@svg/logo.svg";
 export { default as Search } from "@svg/search.svg";
 export { default as Share } from "@svg/share.svg";
 export { default as ShoppingCart } from "@svg/shopping-cart.svg";
-export { default as Star } from "@svg/star.svg"
+export { default as Star } from "@svg/star.svg";
