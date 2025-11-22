f921d8ae2aecbc74f6d3699519a04f05386dddf0
jstar_00 <wltjd6300@naver.com>
2025-11-22 15:12:38 +0900

    feat: BottomSheet 애니메이션 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import BottomSheet from "@/shared/components/bottom-sheet/bottom-sheet";
 
 function App() {
-  return <BottomSheet isOpen={true} />;
+  return (
+    <BottomSheet isOpen={true}>
+      <div>바텀시트</div>
+    </BottomSheet>
+  );
 }
 
 export default App;
--- src/shared/components/bottom-sheet/bottom-sheet.css.ts
+++ src/shared/components/bottom-sheet/bottom-sheet.css.ts
@@ -1,5 +1,17 @@
-import { style } from "@vanilla-extract/css";
+import { style, keyframes } from "@vanilla-extract/css";
 import { color } from "@/shared/styles/tokens/color.css";
+
+// overlay 애니메이션 정의
+const fadeIn = keyframes({
+  from: { opacity: 0 },
+  to: { opacity: 1 },
+});
+
+// sheet 애니메이션 정의
+const slideUp = keyframes({
+  from: { transform: "translateY(100%)" },
+  to: { transform: "translateY(0)" },
+});
 
 // overlay: 전체 바텀 시트의 레이아웃 컨테이너
 // backdrop과 sheet의 부모 컨테이너, flex-end로 sheet를 화면 하단에 배치
@@ -13,6 +25,7 @@ export const overlay = style({
   display: "flex",
   alignItems: "flex-end", // bottom sheet는 화면 아래에서 올라와야 하므로, overlay의 bottom에 위치시켜야 함
   justifyContent: "center",
+  animation: `${fadeIn} 0.2s ease-out`,
 });
 
 // 반투명 검은 배경, 사용자 클릭 시 바텀시트 닫는 용도
@@ -36,6 +49,7 @@ export const sheet = style({
   display: "flex",
   flexDirection: "column",
   zIndex: 1001, // TODO: zIndex 토큰화
+  animation: `${slideUp} 0.3s ease-out`,
 });
 
 export const content = style({
