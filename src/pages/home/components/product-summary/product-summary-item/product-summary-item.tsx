1fe95166882c2056aabde3fc11ade2e025ecb3af
odukong <ohbin1017@gmail.com>
2025-11-23 17:13:01 +0900

    feat : product-summary-item 주석 추가 및 props 타입 수정

--- src/pages/home/components/product-summary/product-summary-item/product-summary-item.tsx
+++ src/pages/home/components/product-summary/product-summary-item/product-summary-item.tsx
@@ -4,20 +4,21 @@ interface ProductSummaryItemProps {
   summary: {
     title: string;
     description: string;
-    readonly badges?: readonly string[];
-    readonly image?: string;
+    badges?: readonly string[];
+    image?: string;
   };
 }
 
 export const ProductSummaryItem = ({ summary }: ProductSummaryItemProps) => {
   return (
     <div className={styles.container}>
+      {/** 상품 요약 정보 각 항목의 제목 */}
       <div className={styles.text({ type: "title" })}>{summary.title}</div>
+      {/** 상품 요약 정보 각 항목의 세부 항목 (설명 - 필수 / 뱃지,이미지 - 선택)*/}
       <div className={styles.information}>
         <div className={styles.text({ type: "description" })}>
           {summary.description}
         </div>
-        {/** badges와 image는 옵션 */}
         {summary.badges && (
           <div className={styles.badgeContainer}>
             {summary.badges.map((badge) => (
