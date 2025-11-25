8693167ee621a3ea3400460d6e1340ba2ed1d047
mimizae <jim400934@gmail.com>
2025-11-25 15:39:01 +0000

    chore: ky 라이브러리 설치

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,12 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+  // Vite에서 사용할 사용자 정의 환경변수 타입 정의
+  readonly VITE_API_BASE_URL: string;
+}
+
+interface ImportMeta {
+  // import.meta.env에 위에서 정의한 타입 연결
+  readonly env: ImportMetaEnv;
+}
