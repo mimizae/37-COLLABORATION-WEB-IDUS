30cb88e85adbb5b00ed69f43143fb099cc9bb5a8
jstar_00 <wltjd6300@naver.com>
2025-11-26 22:48:25 +0900

    refactor: apis 폴더 위치 이동 및 path alias 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "@/App.tsx";
-import QueryProvider from "./shared/apis/query-client.tsx";
+import QueryProvider from "./apis/query-client.tsx";
 import "./shared/styles/global.css.ts";
 
 createRoot(document.getElementById("root")!).render(
