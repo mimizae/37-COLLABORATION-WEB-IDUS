0c4f25368769c04092bc52ac29545cb3fb116f0c
mimizae <jim400934@gmail.com>
2025-11-27 17:50:14 +0000

    fix: 후기 개수 10으로 수정

--- src/shared/components/drag-handler/drag-handler.tsx
+++ src/shared/components/drag-handler/drag-handler.tsx
@@ -2,18 +2,36 @@ import React from "react";
 import * as styles from "./drag-handler.css";
 
 interface Props {
+  // 터치 이벤트
   onTouchStart?: (e: React.TouchEvent) => void;
   onTouchMove?: (e: React.TouchEvent) => void;
   onTouchEnd?: (e: React.TouchEvent) => void;
+  // 마우스 이벤트
+  onMouseDown?: (e: React.MouseEvent) => void;
+  onMouseMove?: (e: React.MouseEvent) => void;
+  onMouseUp?: (e: React.MouseEvent) => void;
+  onMouseLeave?: (e: React.MouseEvent) => void;
 }
 
-const DragHandler = ({ onTouchStart, onTouchMove, onTouchEnd }: Props) => {
+const DragHandler = ({
+  onTouchStart,
+  onTouchMove,
+  onTouchEnd,
+  onMouseDown,
+  onMouseMove,
+  onMouseUp,
+  onMouseLeave,
+}: Props) => {
   return (
     <div
       className={styles.container}
       onTouchStart={onTouchStart}
       onTouchMove={onTouchMove}
-      onTouchEnd={onTouchEnd}>
+      onTouchEnd={onTouchEnd}
+      onMouseDown={onMouseDown}
+      onMouseMove={onMouseMove}
+      onMouseUp={onMouseUp}
+      onMouseLeave={onMouseLeave}>
       <div className={styles.dragHandler}></div>
     </div>
   );
