e440a0db31d583497d5fbb22a1af8c9bb6fd7045
odukong <ohbin1017@gmail.com>
2025-11-27 23:23:24 +0900

    fix : 빌드 에러 해결

--- src/pages/product-info/product-info.tsx
+++ src/pages/product-info/product-info.tsx
@@ -4,9 +4,37 @@ import { Carousel } from "./components/carousel/carousel";
 import { ProductMainInfo } from "./components/product-main-info/product-main-info";
 import { ProductSummary } from "./components/product-summary/product-summary";
 import { useProductInfo } from "@/apis/queries/use-product-info.query";
+import { useAuthorInfoQuery } from "@/apis/queries/use-author-info.query";
 
 export const ProductInfo = () => {
+  const data = {
+    authorName: "SPEDEAR",
+    productName: "[idus단독]음성을 전하는 카세트 앨범 음성 편지",
+    originalPrice: 15000,
+    discountRate: 26,
+    discountedPrice: 11665,
+    averageScore: 4.8,
+    reviewCount: 634,
+    salesCount: 4319,
+  };
   const { data: productInfo } = useProductInfo({ userId: 1, productId: 1 });
+
+  const authorId = 1;
+  const userId = 1;
+
+  const AuthorInfoContent = () => {
+    const { data: author } = useAuthorInfoQuery(authorId, userId);
+
+    return (
+      <MakerInfo
+        name={author?.name ?? data.authorName}
+        description={author?.description ?? "작가 정보를 불러오는 중입니다."}
+        profileImage={author?.imageUrl}
+        liked={author?.isLiked}
+        likeCount={author?.likeCount}
+      />
+    );
+  };
   return (
     <>
       {/** 캐러셀 */}
