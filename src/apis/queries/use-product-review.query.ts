66c0528da8cdfb5712c777e633bdc245b0197e70
mimizae <jim400934@gmail.com>
2025-11-27 14:26:43 +0000

    feat: 후기 조회 api 연동 및 버튼 기능 수정

--- src/apis/queries/use-product-review.query.ts
+++ src/apis/queries/use-product-review.query.ts
@@ -1,1 +1,23 @@
-// 작품 리뷰 조회 API
+// 상품 리뷰 조회 API
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { HTTPMethod, request } from "@/apis/request";
+import { API_ENDPOINTS } from "@/apis/constants/api-endpoints";
+import { productQueryKeys } from "@/apis/constants/query-key";
+import type { ReviewListResponse } from "@/apis/types/product";
+
+const fetchProductReviews = (productId: number) =>
+  request<ReviewListResponse>({
+    method: HTTPMethod.GET,
+    url: API_ENDPOINTS.PRODUCTS.PRODUCT_REVIEW(productId),
+  });
+
+export const useProductReviewQuery = (productId: number) =>
+  useSuspenseQuery({
+    queryKey: productQueryKeys.reviews(productId),
+    queryFn: () => fetchProductReviews(productId),
+    retry: false,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
+    staleTime: 5 * 60 * 1000,
+  });
