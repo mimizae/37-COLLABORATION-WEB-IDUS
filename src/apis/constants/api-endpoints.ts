f2ab47621f253999bde12f91b10663eafe3ba05c
mimizae <jim400934@gmail.com>
2025-11-27 12:10:26 +0000

    api: 작품 좋아요 api 구현 및 연동

--- src/apis/constants/api-endpoints.ts
+++ src/apis/constants/api-endpoints.ts
@@ -1,11 +1,11 @@
 export const API_ENDPOINTS = {
   AUTHORS: {
-    AUTHOR_INFO: (authorId: number) => `/authors/${authorId}`,
-    AUTHOR_LIKES: (authorId: number) => `/authors/${authorId}/likes`,
+    AUTHOR_INFO: (authorId: number) => `authors/${authorId}`,
+    AUTHOR_LIKES: (authorId: number) => `authors/${authorId}/likes`,
   },
   PRODUCTS: {
-    PRODUCT_INFO: (productId: number) => `/products/${productId}`,
-    PRODUCT_REVIEW: (productId: number) => `/products/${productId}/reviews`,
-    PRODUCT_LIKES: (productId: number) => `/products/${productId}/likes`,
+    PRODUCT_INFO: (productId: number) => `products/${productId}`,
+    PRODUCT_REVIEW: (productId: number) => `products/${productId}/reviews`,
+    PRODUCT_LIKES: (productId: number) => `products/${productId}/likes`,
   },
 } as const;
